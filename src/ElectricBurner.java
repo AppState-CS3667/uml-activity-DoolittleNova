cf42960e3c04eb2805396dbbf798ac81563c1937
DoolittleNova <doolittletr@appstate.edu>
2024-09-20 09:47:05 -0400

    Implemented ElectricBurner.java

--- src/ElectricBurner.java
+++ src/ElectricBurner.java
@@ -1,7 +1,13 @@
 public class ElectricBurner extends Burner {
+    private double voltReq, ampReq;
 
     public ElectricBurner(double maxTemp, double minTemp, double diameter, double voltReq) {
         super(maxTemp, minTemp, diameter);
+        this.voltReq = voltReq;
     }
+
+    public void increaseCurrent() {}
+    public void decreaseCurrent() {}
+    public double getVoltReq() { return this.voltReq; }
     
 }
